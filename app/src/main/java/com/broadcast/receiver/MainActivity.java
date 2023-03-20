bf6c71104eb067af6160045de79b5f5eee9e7074
Macca5357 <19237766@studentmail.ul.ie>
2023-03-20 23:34:08 +0000

    UI Completed

---
+++ app/src/main/java/com/broadcast/receiver/MainActivity.java
@@ -0,0 +1,42 @@
+/* Student name: Gavin McCarthy
+ * Student id: 19237766
+ */
+package com.broadcast.receiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+     Button time_trigger_button,notification_trigger_button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        time_trigger_button=findViewById(R.id.time_trigger_button);
+        notification_trigger_button=findViewById(R.id.notification_trigger_button);
+        time_trigger_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Time trigger button clicked", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+     notification_trigger_button.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+             Toast.makeText(MainActivity.this, "Notification trigger button clicked", Toast.LENGTH_SHORT).show();
+         }
+     });
+    }
+}
